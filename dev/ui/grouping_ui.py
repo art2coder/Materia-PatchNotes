baf5e565dca0cbefbd11ff566baac9c252489683
art2coder <garseck@gmail.com>
2025-05-01 05:15:16 +0900

    Update grouping_ui.py

--- dev/ui/grouping_ui.py
+++ dev/ui/grouping_ui.py
@@ -1,11 +1,10 @@
 import bpy
-from ..operators.grouping import OBJECT_OT_group_by_empty, OBJECT_OT_ungroup_empty
 
 def menu_grouping(self, context):
     layout = self.layout
     layout.separator()
-    layout.operator(OBJECT_OT_group_by_empty.bl_idname, icon='OUTLINER_OB_EMPTY')
-    layout.operator(OBJECT_OT_ungroup_empty.bl_idname,   icon='X')
+    layout.operator("object.group_by_empty", icon='OUTLINER_OB_EMPTY')
+    layout.operator("object.ungroup_empty",   icon='X')
 
 def register():
     bpy.types.VIEW3D_MT_object.append(menu_grouping)
