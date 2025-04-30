b3c7a00524bd74603293059fdbeae3fd74c59015
art2coder <garseck@gmail.com>
2025-05-01 03:50:44 +0900

    Update keymap.py

--- dev/utils/keymap.py
+++ dev/utils/keymap.py
@@ -1,4 +1,3 @@
-# utils/keymap.py
 import bpy
 
 addon_keymaps = []
@@ -8,17 +7,33 @@ def register_keymaps():
     kc = wm.keyconfigs.addon
     if not kc:
         return
-    km = kc.keymaps.new(name='Object Mode', space_type='EMPTY')
-    # 기존 Ctrl+G, Alt+G 제거 및 새 매핑 추가
-    for kmi in list(km.keymap_items):
+
+    # 1) Object Mode: Ctrl+G / Alt+G (Grouping / Ungrouping)
+    km_obj = kc.keymaps.new(name='Object Mode', space_type='EMPTY')
+    # --- remove existing conflicts ---
+    for kmi in list(km_obj.keymap_items):
         if kmi.type == 'G' and kmi.ctrl and not kmi.alt:
-            km.keymap_items.remove(kmi)
+            km_obj.keymap_items.remove(kmi)
         if kmi.type == 'G' and kmi.alt and not kmi.ctrl:
-            km.keymap_items.remove(kmi)
-    kmi = km.keymap_items.new('object.group_by_empty','G','PRESS',ctrl=True)
-    addon_keymaps.append((km, kmi))
-    kmi = km.keymap_items.new('object.ungroup_empty','G','PRESS',alt=True)
-    addon_keymaps.append((km, kmi))
+            km_obj.keymap_items.remove(kmi)
+    # --- new mappings ---
+    kmi = km_obj.keymap_items.new(
+        'object.group_by_empty', type='G', value='PRESS', ctrl=True, alt=False
+    )
+    addon_keymaps.append((km_obj, kmi))
+    kmi = km_obj.keymap_items.new(
+        'object.ungroup_empty', type='G', value='PRESS', ctrl=False, alt=True
+    )
+    addon_keymaps.append((km_obj, kmi))
+
+    # 2) Global Window: Q → Pie Menu
+    km_win = kc.keymaps.new(name='Window', space_type='EMPTY')
+    kmi = km_win.keymap_items.new(
+        'wm.call_menu_pie', type='Q', value='PRESS'
+    )
+    kmi.properties.name = "PIE_MT_modifiers_pie"
+    addon_keymaps.append((km_win, kmi))
+
 
 def unregister_keymaps():
     for km, kmi in addon_keymaps:
