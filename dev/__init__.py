1702ed45a7a50e496140cdea7c7c538c7013f20e
art2coder <garseck@gmail.com>
2025-05-01 05:45:19 +0900

    Update __init__.py

--- dev/__init__.py
+++ dev/__init__.py
@@ -1,39 +1,27 @@
 bl_info = {
     "name": "Modifier Pie Kit",
-    "author": "Your Name",
+    "author": "EJ",
     "version": (1, 1, 7),
-    "blender": (4, 2, 0),     
+    "blender": (4, 2, 0),
     "location": "View3D > Object > Modifier Pie Kit",
     "description": "A pie of modifier shortcuts and rotational array",
-    "warning": "",
-    "wiki_url": "",
-    "tracker_url": "",
     "category": "Object",
 }
 
 import bpy
 from . import ui
-from .operators import grouping
-from .operators import popup_modifiers, pie_menu
 from .operators import register as register_ops, unregister as unregister_ops
 from .utils.keymap import register_keymaps, unregister_keymaps
 
-
 def register():
-    popup_modifiers.register()
-    pie_menu.register()
-    grouping.register()
+    register_ops()          
     ui.register()
     register_keymaps()
-    register_ops()
 
 def unregister():
-    unregister_ops()
     unregister_keymaps()
     ui.unregister()
-    grouping.unregister()
-    pie_menu.register()
-    popup_modifiers.register()
+    unregister_ops()
 
 if __name__ == "__main__":
     register()
