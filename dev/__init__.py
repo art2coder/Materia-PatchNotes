8af83df77e29aa05f621c29885ad28db495ec33a
art2coder <garseck@gmail.com>
2025-04-30 17:35:38 +0900

    Update __init__.py

--- dev/__init__.py
+++ dev/__init__.py
@@ -1,1 +1,20 @@
-# 초기 구조화 준비용 파일
+bl_info = {
+    "name": "Modifier Pie Kit",
+    "blender": (1, 1, 7),
+    "category": "Object",
+}
+
+import bpy
+from .operators import grouping
+from . import ui
+
+def register():
+    grouping.register()
+    ui.register()
+
+def unregister():
+    ui.unregister()
+    grouping.unregister()
+
+if __name__ == "__main__":
+    register()
