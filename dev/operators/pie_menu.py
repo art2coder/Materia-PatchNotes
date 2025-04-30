eafcca6115bf1fa47586af6c0ebd344cc6dda931
art2coder <garseck@gmail.com>
2025-05-01 06:44:54 +0900

    Update pie_menu.py

--- dev/operators/pie_menu.py
+++ dev/operators/pie_menu.py
@@ -17,17 +17,17 @@ class PIE_MT_modifiers_pie(bpy.types.Menu):
         pie.operator("modifier_pie.mirror_live_popup",         text="Mirror",       icon='MOD_MIRROR')
    
         # ↘ Bottom-Right
-        pie.operator("modifier_pie.curve_bevel_popup",         text="Curve Bevel",  icon='CURVE_DATA') 
+        pie.operator("modifier_pie.curve_bevel_popup",         text="Curve Bevel",  icon='CURVE_DATA')
 
-        # ↙ Bottom-Left
-        pie.operator("modifier_pie.apply_all_common_modifiers", text="Apply All",   icon='FILE_TICK')
+            # ↖ Top-Left
+        pie.operator("view3d.toggle_overlay",            text="Toggle Overlay", icon='OVERLAY')
    
         # ← Left
         pie.operator("modifier_pie.toggle_display_wire",       text="Toggle Wire",  icon='SHADING_WIRE')
 
-        # ↖ Top-Left
-        pie.operator("view3d.toggle_overlay",            text="Toggle Overlay", icon='OVERLAY')
-        
+         # ↙ Bottom-Left
+        pie.operator("modifier_pie.apply_all_common_modifiers", text="Apply All",   icon='FILE_TICK')
+
         # ↓ Bottom
         pie.operator("modifier_pie.add_subsurf_popup",         text="Subsurf",      icon='MOD_SUBSURF')
 
