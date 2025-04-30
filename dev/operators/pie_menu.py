7305e920fbb8cc100ee79c074f9ea302ae3dae44
art2coder <garseck@gmail.com>
2025-05-01 04:42:30 +0900

    Update pie_menu.py

--- dev/operators/pie_menu.py
+++ dev/operators/pie_menu.py
@@ -8,25 +8,25 @@ class PIE_MT_modifiers_pie(bpy.types.Menu):
     pie = self.layout.menu_pie()
 
     # ↑ Top
-    pie.operator("object.add_boolean_popup",         text="Boolean",      icon='MOD_BOOLEAN')
+    pie.operator("modifier_pie.add_boolean_popup",         text="Boolean",      icon='MOD_BOOLEAN')
     
     # ↗ Top-Right
-    pie.operator("object.add_bevel_popup",           text="Bevel",        icon='MOD_BEVEL')
+    pie.operator("modifier_pie.add_bevel_popup",           text="Bevel",        icon='MOD_BEVEL')
   
     # → Right
-    pie.operator("object.mirror_live_popup",         text="Mirror",       icon='MOD_MIRROR')
+    pie.operator("modifier_pie.mirror_live_popup",         text="Mirror",       icon='MOD_MIRROR')
    
     # ↘ Bottom-Right
-    pie.operator("object.curve_bevel_popup",         text="Curve Bevel",  icon='CURVE_DATA')
+    pie.operator("modifier_pie.curve_bevel_popup",         text="Curve Bevel",  icon='CURVE_DATA')
 
     # ↓ Bottom
-    pie.operator("object.add_subsurf_popup",         text="Subsurf",      icon='MOD_SUBSURF')
+    pie.operator("modifier_pie.add_subsurf_popup",         text="Subsurf",      icon='MOD_SUBSURF')
    
     # ↙ Bottom-Left
-    pie.operator("object.apply_all_common_modifiers", text="Apply All",   icon='FILE_TICK')
+    pie.operator("modifier_pie.apply_all_common_modifiers", text="Apply All",   icon='FILE_TICK')
 
     # ← Left
-    pie.operator("object.toggle_display_wire",       text="Toggle Wire",  icon='SHADING_WIRE')
+    pie.operator("modifier_pie.toggle_display_wire",       text="Toggle Wire",  icon='SHADING_WIRE')
 
     # ↖ Top-Left
     pie.operator("view3d.toggle_overlay",            text="Toggle Overlay", icon='OVERLAY')
