e69a7771a6a359411cfb49ffd60f1700d828c219
art2coder <garseck@gmail.com>
2025-05-01 04:12:15 +0900

    Create pie_menu.py

---
+++ dev/operators/pie_menu.py
@@ -0,0 +1,42 @@
+import bpy
+
+class PIE_MT_modifiers_pie(bpy.types.Menu):
+    bl_label = "Modifier Pie Menu"
+    bl_idname = "PIE_MT_modifiers_pie"
+
+    def draw(self, context):
+    pie = self.layout.menu_pie()
+
+    # ↑ Top
+    pie.operator("object.add_boolean_popup",         text="Boolean",      icon='MOD_BOOLEAN')
+    
+    # ↗ Top-Right
+    pie.operator("object.add_bevel_popup",           text="Bevel",        icon='MOD_BEVEL')
+  
+    # → Right
+    pie.operator("object.mirror_live_popup",         text="Mirror",       icon='MOD_MIRROR')
+   
+    # ↘ Bottom-Right
+    pie.operator("object.curve_bevel_popup",         text="Curve Bevel",  icon='CURVE_DATA')
+
+    # ↓ Bottom
+    pie.operator("object.add_subsurf_popup",         text="Subsurf",      icon='MOD_SUBSURF')
+   
+    # ↙ Bottom-Left
+    pie.operator("object.apply_all_common_modifiers", text="Apply All",   icon='FILE_TICK')
+
+    # ← Left
+    pie.operator("object.toggle_display_wire",       text="Toggle Wire",  icon='SHADING_WIRE')
+
+    # ↖ Top-Left
+    pie.operator("view3d.toggle_overlay",            text="Toggle Overlay", icon='OVERLAY')
+
+        
+
+classes = (PIE_MT_modifiers_pie,)
+
+def register():
+    bpy.utils.register_class(PIE_MT_modifiers_pie)
+
+def unregister():
+    bpy.utils.unregister_class(PIE_MT_modifiers_pie)
