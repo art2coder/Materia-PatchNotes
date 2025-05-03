4a30a40fc8a6067a3e4c3b5829da7f7d4fd462b8
art2coder <garseck@gmail.com>
2025-05-04 08:05:05 +0900

    Update clean_view.py
    컬러가 오브젝트로 수정되지 않아서 다시 수정

--- dev/operators/clean_view.py
+++ dev/operators/clean_view.py
@@ -36,9 +36,9 @@ def apply_clean_view_settings(space):
     space.overlay.show_overlays = False
 
 def restore_clean_view_settings(space, settings):
-    space.shading.type = settings.get('type', 'SOLID')
+        space.shading.type = settings.get('type', 'SOLID')
     space.shading.background_type = settings['background_type']
-    space.shading.color_type = 'OBJECT'
+    space.shading.color_type = 'OBJECT'  # 명시적으로 오브젝트로 설정
     space.shading.light = settings['light']
     space.shading.background_color = settings['background_color']
     space.overlay.show_overlays = settings['show_overlays']
