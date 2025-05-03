8c0c8bebe734e0b2a0cd0e9467efb778235dc258
art2coder <garseck@gmail.com>
2025-05-03 18:59:18 +0900

    Update popup_modifiers.py

--- dev/operators/popup_modifiers.py
+++ dev/operators/popup_modifiers.py
@@ -31,7 +31,7 @@ class OBJECT_OT_rotational_array(bpy.types.Operator):
         cursor = context.scene.cursor.location.copy()
 
         # 1) Apply transforms, set origin to cursor
-        bpy.ops.object.transform_apply(scale=True)
+        bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
         
 
         # 2) Create empty for parenting (ARROWS)
