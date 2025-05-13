7fa050c10d97974e0d19acdc80d2f7c4690bfaaf
art2coder <garseck@gmail.com>
2025-05-13 17:02:40 +0900

    Update __init__.py

--- dev/__init__.py
+++ dev/__init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Modifier Pie Kit",
     "author": "EJ",
-    "version": (1, 2, 0),
+    "version": (1, 2, 1),
     "blender": (4, 2, 0),
     "location": "View3D > Object > Modifier Pie Kit",
     "description": "A pie of modifier shortcuts and rotational array",
@@ -20,12 +20,16 @@ bl_info = {
 # v1.1.8 - Added Rotational array 
 # v1.1.9 - Added Select Tool (B)
 # v1.2.0 - Added Extras Tool
+# v1.2.1 - Added Move Bottom
 
 
 import bpy
 from . import ui
 from .operators import register as register_ops, unregister as unregister_ops
 from .utils.keymap import register_keymaps, unregister_keymaps
+from . import extras
+
+USE_EXTRAS = True                        # ✅ 필요할 때만 True로 설정
 
 
 def register():
@@ -33,7 +37,13 @@ def register():
     ui.register()
     register_keymaps()
 
+    if USE_EXTRAS:                       # ✅ 선택적 등록
+        extras.register()
+
 def unregister():
+    if USE_EXTRAS:
+        extras.unregister()
+       
     unregister_keymaps()
     ui.unregister()
     unregister_ops()
