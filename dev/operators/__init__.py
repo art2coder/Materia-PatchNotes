6b9c0dc4b709f5aef575c6ecb50257a0787eb392
art2coder <garseck@gmail.com>
2025-08-11 20:47:32 +0900

    Update __init__.py

--- dev/operators/__init__.py
+++ dev/operators/__init__.py
@@ -2,35 +2,42 @@
 
 from .grouping           import register as register_group,        unregister as unregister_group
 from .popup_modifiers    import register as register_mods,         unregister as unregister_mods
-from .pie_modifiers      import register as register_pie,          unregister as unregister_pie
 from .popup_pivot        import register as register_pivot_ops,    unregister as unregister_pivot_ops
 from .pie_pivot          import register as register_pivot,        unregister as unregister_pivot
-from .tools_extra import register as register_select, unregister as unregister_select
-from . import auto_sorter
+from .boolean            import register as register_boolean,      unregister as unregister_boolean
+from .preferences        import register as register_preferences,  unregister as unregister_preferences 
+from .array_modifier_popup import register as register_array_popup, unregister as unregister_array_popup
+
+
 from . import clean_view
 from . import camera_quick_settings
-from . import lineart_extras
+from . import Outliner_Enhancer
+
 
 def register():
     register_group()
-    register_mods()
-    register_pie()
+    register_mods()    
     register_pivot_ops()   
     register_pivot()
-    register_select()
-    auto_sorter.register()
+    register_boolean()
+    register_preferences()
+    register_array_popup()
+    
     clean_view.register()
     camera_quick_settings.register()
-    lineart_extras.register()
+    Outliner_Enhancer.register()
+    
 
 def unregister():
-    lineart_extras.unregister()
+    
+    Outliner_Enhancer.unregister()
     camera_quick_settings.unregister()
     clean_view.unregister()
-    auto_sorter.unregister()
-    unregister_select()
+    
+    unregister_array_popup()
+    unregister_preferences()
+    unregister_boolean()
     unregister_pivot()
-    unregister_pivot_ops()  
-    unregister_pie()
+    unregister_pivot_ops()    
     unregister_mods()
     unregister_group()
